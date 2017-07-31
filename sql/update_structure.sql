5a8c2c04b64a3bce08aaa0887d3809c864070a8b
Michal Landsman <landsman@insuit.cz>
2017-07-31 16:18:22 +0200

    admin done

--- sql/update_structure.sql
+++ sql/update_structure.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `oc_option_value`
-ADD `is_default` int(1) NULL COMMENT 'option will be selected by default in controller if will alone';
+ADD `is_default` int(1) NOT NULL DEFAULT '0' COMMENT 'option will be selected by default in controller if will alone';
